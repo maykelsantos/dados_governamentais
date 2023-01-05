2896c79b492f52cb6b51607b5ceced715a903120
Maykel Santos <eumaykelandre@gmail.com>
2023-01-05 01:19:12 -0300

    ainda em análises

--- portal_transparencia/analises.py
+++ portal_transparencia/analises.py
@@ -19,4 +19,9 @@ c_dataEmissao = 'dataEmissao'
 c_numero = 'numero'
 c_serie = 'serie'
 
-print(base_dados[c_valorNotaFiscal])
+# convertendo dtype da coluna 'valorNotaFiscal' para float
+base_dados[c_valorNotaFiscal] = base_dados[c_valorNotaFiscal].apply(lambda x: str(x).replace('.', '').replace(',', '.'))
+base_dados[c_valorNotaFiscal] = base_dados[c_valorNotaFiscal].astype('float64')
+
+
+print(base_dados.columns[c_nomeFornecedor])
